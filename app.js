6e8a845d127aa76a854cf83960938341d040d7d7
Oliver Norgrove <norgrovework@gmail.com>
2024-01-01 20:04:31 +0000

    java insert

---
+++ app.js
@@ -0,0 +1,19 @@
+// template_5s7x2vn
+// service_hdf5ys6
+// hHPdSwEXfYS2rGity
+
+function contact(event) {
+    event.preventDefault();
+    // emailjs
+      //  .sendForm(
+        //    'service_hdf5ys6',
+          //  'template_5s7x2vn',
+            //event.target,
+            //'hHPdSwEXfYS2rGity'
+        //).then(() => {
+            console.log('this worked1')
+        })
+        const loading = document.querySelector('.modal__overlay--loading');
+        const success = document.querySelector('.modal__overlay--success');
+        loading.classList += " modal__overlay--visible"
+}
