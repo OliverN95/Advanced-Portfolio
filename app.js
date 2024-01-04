735d34ed7503274ffe29c9e08a4fcacd195ebd61
Oliver Norgrove <norgrovework@gmail.com>
2024-01-04 22:57:39 +0000

    final layout minus logo

--- app.js
+++ app.js
@@ -1,19 +1,61 @@
-// template_5s7x2vn
-// service_hdf5ys6
-// hHPdSwEXfYS2rGity
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1 / 20;
+
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX / 20; 
+  const y = event.clientY / 20;
+  
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const oddInteger = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * oddInteger}px, ${y * oddInteger}px)`
+  }
+  }
+
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-      //  .sendForm(
-        //    'service_hdf5ys6',
-          //  'template_5s7x2vn',
-            //event.target,
-            //'hHPdSwEXfYS2rGity'
-        //).then(() => {
-            console.log('this worked1')
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+     emailjs
+        .sendForm(
+            'service_hdf5ys6',
+            'template_5s7x2vn',
+            event.target,
+            'hHPdSwEXfYS2rGity'
+        ).then(() => {
+          loading.classList.remove("modal__overlay--visible");
+          success.classList += " modal__overlay--visible"  
+        }).catch(() => {
+          loading.classList.remove("modal__overlay--visible");
+          alert(
+            "This email service is temporarily unavailable. Please contact me at norgrovework@gmail.com"
+          );
         })
-        const loading = document.querySelector('.modal__overlay--loading');
-        const success = document.querySelector('.modal__overlay--success');
-        loading.classList += " modal__overlay--visible"
+}
+
+function toggleModal() {
+
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  
+  else isModalOpen = true;
+  document.body.classList += " modal--open";
 }
